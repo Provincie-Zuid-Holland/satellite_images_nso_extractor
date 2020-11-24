19c2c444dcd11ffa6037d75893e932daa93cb3a9
Winter <mr.de.winter@pzh.nl>
2020-11-24 15:33:23 +0100

    init

---
+++ src/satellite_images_nso/_nso_data_extraction/nso_api.py
@@ -0,0 +1,129 @@
+import requests
+from requests.auth import HTTPBasicAuth 
+import json
+import geopandas as gpd
+import objectpath
+import os
+from datetime import date
+import zipfile 
+import shapely
+import numpy as np
+
+
+def  __getFeatures(gdf):
+    """Function to parse features from GeoDataFrame in such a manner that rasterio wants them
+    
+        @param gdf: a geopandas data frame
+    """
+    return [json.loads(gdf.to_json())['features'][0]['geometry']['coordinates']]
+
+
+def retrieve_download_links(path_to_geojson, user_n, pass_n, start_date = "2014-01-01", end_date =date.today().strftime("%Y-%m-%d"),max_meters=3,):
+    """
+        This functions retrieves download links for area chosen in the geojson.
+
+        @param path_to_geojson: path to where the geojson is stored.
+        @param start_date: From when satelliet date needs to be looked at.
+        @param end_date: the end date of the period which needs to be looked at
+        @param max_meters: Maximum resolution which needs to be looked at.
+
+        @return: the found download links.
+
+    """
+
+    geojson_coordinates = __getFeatures(gpd.read_file(path_to_geojson))[0]
+
+    url = 'https://api.satellietdataportaal.nl/v1/search'
+    myobj = { "type": "Feature","geometry":
+    {"type": "Polygon", "coordinates": geojson_coordinates },"properties": {"fields":{"geometry":"false"},"filters" :
+    { "datefilter":{ "startdate": start_date,"enddate" : end_date }, "resolutionfilter":{ "maxres" :max_meters, "minres" : 0 }} } }
+
+    headers = {'content-type': 'application/json'}
+    x = requests.post(url, auth = HTTPBasicAuth(user_n, pass_n), data = json.dumps(myobj) ,  headers=headers )
+
+    reponse = json.loads(x.text)
+
+    links = []
+
+    for row in reponse['features']:
+        try:  
+            if row['properties']['downloads'] is not None:
+
+                # Check for cloudcoverage. TODO: Use a variable for it.
+                if row['properties']['cloudcover'] is None or float(row['properties']['cloudcover']) < 30.0 :
+                    # This checks to see if the geojson is in the full region. TODO: Make it optional and propertional
+                    if check_if_geojson_in_region(row,geojson_coordinates ) == True:                
+                        for download in row['properties']['downloads']:
+                            links.append(download['href'] )
+                
+        except Exception as e: 
+            print(e)
+            print(row)
+
+    return links
+
+
+def download_link(link,absolute_path, user_n, pass_n):
+    """
+        Method for downloading satelliet data from a link.
+
+        @param link: a link where the satelliet data is stored.
+        @param absolute_path: the filename and path where the file will get downloaded.
+    """
+
+    r = requests.get(link,auth = HTTPBasicAuth(user_n, pass_n))
+    #retrieving data from the URL using get method
+    with open(absolute_path, 'wb') as f:
+        #giving a name and saving it in any required format
+        #opening the file in write mode
+        f.write(r.content)
+
+def unzip_delete(path,delete = True): 
+    """
+        Unzip a zip file and delete the .zip file.
+    """
+    with zipfile.ZipFile(path, 'r') as zip_ref:
+        zip_ref.extractall(path.replace(".zip",""))
+    
+    if delete == True:
+        os.remove(path)
+
+    return path.replace(".zip","")
+
+
+def check_if_geojson_in_region(row,projected_shape):
+    """
+        This method checks if the geojson is fully in the TCI raster.
+
+        TODO: For now it's only fully in the georegion.        
+        @param src_TCI_raster_dataset: The rasterio opened TCI raster.
+        @param projected_shape: The geojson warped to the src of the copernicus crs.
+    """
+    
+    return_statement = False 
+    
+    row_shape = shapely.geometry.Polygon([[coordx[0],coordx[1]] for coordx in row['geometry']['coordinates'][0]])
+    geojson_shape = shapely.geometry.Polygon([[coordx[0],coordx[1]] for coordx in projected_shape[0]])
+    geojson_shape_xy = geojson_shape.boundary.xy
+
+    xy_intersection = geojson_shape.intersection(row_shape).boundary.xy
+    
+    #print(geojson_shape.within(row_shape) )
+    #print(np.array(geojson_shape_xy).shape)
+    #print(np.array(xy_intersection).shape )
+    try:
+        difference_array = np.array(geojson_shape_xy) -  np.array(xy_intersection)
+       
+        if max(difference_array.min(), difference_array.max(), key=abs) < 10000:
+            return_statement = True
+    except:
+        print(" ")
+        #print(row_shape.intersection(geojson_shape) )
+
+    #print(return_statement)
+    
+    return return_statement
+
+
+    
+
---
+++ src/satellite_images_nso/api/nso_georegion.py
@@ -0,0 +1,105 @@
+import os
+import satellite_images_nso._nso_data_extraction.nso_api as nso_api
+import satellite_images_nso._tif_cutter.nso_cutter as nso_cutter
+import satellite_images_nso._encryption.encryption as encryption
+from datetime import date
+import glob
+import shutil
+
+
+
+class nso_georegion:
+
+    def __init__(self, path_to_geojson: str, output_folder: str, username: str, password: str):
+        """
+            Init of the class.
+
+            @path_to_geojson: Path where the geojson is located.
+            @output_folder: folder where the cropped and nvdi files will be stored.
+            @username: the username of the nso account.
+            @password: the password of the nso account
+        """
+        self.path_to_geojson = path_to_geojson
+        self.output_folder = output_folder 
+       
+        self.username = username
+        self.password = password
+
+    def retrieve_download_links(self,start_date = "2014-01-01", end_date =date.today().strftime("%Y-%m-%d"),max_meters=3):
+        """
+            This functions retrieves download links for area chosen in the geojson for the nso.
+
+            @param path_to_geojson: path to where the geojson is stored.
+            @param start_date: From when satelliet date needs to be looked at.
+            @param end_date: the end date of the period which needs to be looked at
+            @param max_meters: Maximum resolution which needs to be looked at.
+
+            @return: the found download links.
+        """
+        return nso_api.retrieve_download_links(self.path_to_geojson,self.username, self.password, start_date = "2014-01-01", end_date =date.today().strftime("%Y-%m-%d"),max_meters=3  )
+
+    def crop_and_calculate_nvdi(self,path):
+        """
+            Function for the crop and the calculating of the NVDI index.
+            Can be used as a standalone if you have already unzipped the file.
+
+            @oaram path: Path to a .tif file.
+            @param download_archive_name: name of the zip file.
+            @param delete_zip_file: wether or not to delete the downloaded .zip file.
+            @param delete_source_files: wether or not to delete the unzipped file.
+        """
+
+        true_path = path
+        if '.tif' not in true_path:
+            for x in glob.glob(path+'/*.tif', recursive=True):
+                true_path = x
+        
+        if ".tif" not in true_path:
+            return ".Tif not found"
+        else: 
+            cropped_path, nvdi_path, nvdi_matrix =  nso_cutter.run(true_path, self.path_to_geojson )
+            cropped_path = cropped_path.replace("\\", "/") 
+            nvdi_path = nvdi_path.replace("\\", "/")
+            nvdi_matrix = nvdi_matrix.replace("\\","/")
+            shutil.move(cropped_path,self.output_folder+cropped_path.split("/")[len(cropped_path.split("/"))-1])
+            shutil.move(nvdi_path,self.output_folder+nvdi_path.split("/")[len(nvdi_path.split("/"))-1])
+            shutil.move(nvdi_matrix,self.output_folder+nvdi_matrix.split("/")[len(nvdi_matrix.split("/"))-1])
+
+            
+    def execute_link(self, link, delete_zip_file = True, delete_source_files = True):
+        """ 
+            Executes the download, croppend and the calculating of the NVDI for a specific link.
+        
+            @param link: Link to a file from the NSO.
+            @param geojson_path: Path to a geojson with the selected region.
+            @param delete_zip_file: whether or not to keep the original .zip file.
+            @param delete_source_files: whether or not to keep the extracted files.
+        """
+        download_archive_name = self.output_folder+"/"+link.split("/")[len(link.split("/"))-1]+"_"+link.split("/")[len(link.split("/"))-2]+'.zip'
+
+        nso_api.download_link(link,download_archive_name, self.username, self.password)
+        extracted_folder = nso_api.unzip_delete(download_archive_name,False)
+
+        self.crop_and_calculate_nvdi(extracted_folder)
+    
+        if delete_zip_file == True:
+            os.remove(download_archive_name)
+
+        if delete_source_files == True:
+            shutil.rmtree(extracted_folder)
+
+        return "Succesfully cropped .tif file"
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
