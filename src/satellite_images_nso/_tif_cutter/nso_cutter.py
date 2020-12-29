4137aa70e2bab5723ef82378a2d989e255e062b1
yilongwen <yilong.wen@twyz.nl>
2020-12-29 14:09:01 +0100

    Updated nso_cutter.py
    - Added method to create a pandas dataframe with (x, y) coordinates and a point geometry column

--- src/satellite_images_nso/_tif_cutter/nso_cutter.py
+++ src/satellite_images_nso/_tif_cutter/nso_cutter.py
@@ -1,6 +1,7 @@
 import rasterio
 from rasterio.plot import show
 import geopandas as gpd
+from pandas import DataFrame
 import json
 import shapely
 import pyproj
@@ -32,11 +33,11 @@ def __make_the_cut(geo_region, raster_path, raster_path_cropped):
         @param raster_path_cropped: path were the cropped raster will be stored.
     """
 
-    
+
     coords = geo_region
-     
+
     src = rasterio.open(raster_path)
-    
+
     # Change the crs to rijks driehoek, because all the satelliet images are in rijks driehoek
     if geo.crs['init'] != 'epsg:28992':
         geo = geo.to_crs(epsg=28992)
@@ -48,7 +49,7 @@ def __make_the_cut(geo_region, raster_path, raster_path_cropped):
                  "height": out_image.shape[1],
                  "width": out_image.shape[2],
                  "transform": out_transform})
-    
+
     with rasterio.open(raster_path_cropped, "w", **out_meta) as dest:
             dest.write(out_image)
             dest.close()
@@ -63,6 +64,24 @@ def __make_the_cut(geo_region, raster_path, raster_path_cropped):
     rasterio.plot.show(plot_out_image,
           transform=src.transform)
 
+#===============================================================================================
+def __get_xy_df(satelliet_beeld) -> DataFrame:
+    """
+    Maps a rasterio satellite object to pandas dataframe. With the corresponding x and y coordinates
+    @param satelliet_beeld: rasterio DatasetReader
+    @return pandas dataframe: with x and y coordinates in epsg:4326
+    """
+    out_image = satelliet_beeld.read()
+    x, y = satelliet_beeld.xy(list(range(out_image.shape[1])), list(range(out_image.shape[2])))  # Convert indices to x,y. E.g. (0, 0) -> (51320, 418920)
+    satelliet_df = DataFrame(data=[out_image[i].flatten() for i in range(out_image.shape[0])]).T               # Create pandas dataframe with flatten values
+    satelliet_df['x'] = x
+    satelliet_df['y'] = y
+    satelliet_df.columns = ['blue', 'green', 'red', 'nir', 'x', 'y']
+    satelliet_df['geometry'] = gpd.points_from_xy(x=satelliet_df['x'], y=satelliet_df['y'], crs=satelliet_beeld.crs)
+    
+    return satelliet_df
+
+#===============================================================================================
 def __calculate_nvdi_function(raster_path_cropped,raster_path_nvdi):
     """
         Function which calculates the NVDI index, used in a external package.
@@ -71,12 +90,12 @@ def __calculate_nvdi_function(raster_path_cropped,raster_path_nvdi):
         @param raster_path_nvdi: path where the nvdi will be stored.
     """
     src = rasterio.open(raster_path_cropped)
-    data_ndvi = calculate_nvdi.normalized_diff(src.read()[3], src.read()[2]) 
+    data_ndvi = calculate_nvdi.normalized_diff(src.read()[3], src.read()[2])
     data_ndvi.dump(raster_path_nvdi)
-    
+
     return calculate_nvdi.make_ndvi_plot(raster_path_nvdi,raster_path_nvdi)
 
-
+#===============================================================================================
 def run(raster_path,load_shape, calculate_nvdi = True):
     """
         Main run method, combines the cutting of the file based on the shape and calculates the NVDI index.
@@ -93,4 +112,3 @@ def run(raster_path,load_shape, calculate_nvdi = True):
         raster_path_nvdi = raster_path.replace(".tif","_"+load_shape.split("/")[len(load_shape.split("/"))-1].split('.')[0]+"_cropped_nvdi")
         nvdi_output = __calculate_nvdi_function(raster_path_cropped,raster_path_nvdi)
         return raster_path_cropped, nvdi_output, raster_path_nvdi
-    
