ac3e25a51b803ce95e0abaaffa7be7535292c2b0
Winter <mr.de.winter@pzh.nl>
2020-12-31 14:05:50 +0100

    Working pixel geodataframe

--- src/satellite_images_nso/api/sat_manipulator.py
+++ src/satellite_images_nso/api/sat_manipulator.py
@@ -1,6 +1,8 @@
 # Rename to nso_manipulator for more apporiate name.
 import satellite_images_nso._manipulation.nso_manipulator as nso_manipulator
 
-def tranform_vector_to_xy_df(path_to_vector):
-
-    return nso_manipulator.tranform_vector_to_xy_df(path_to_vector )
+def tranform_vector_to_pixel_df(path_to_vector):
+    """
+        Public wrapper around the nso manipulator.
+    """
+    return nso_manipulator.tranform_vector_to_pixel_df(path_to_vector )
