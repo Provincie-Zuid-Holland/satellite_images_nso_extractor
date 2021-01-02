4ae6e63c24d2b7ee97e0670db00976f5050367e3
Winter <mr.de.winter@pzh.nl>
2021-01-02 12:34:22 +0100

    Even more racftors

--- src/satellite_images_nso/api/sat_manipulator.py
+++ src/satellite_images_nso/api/sat_manipulator.py
@@ -3,6 +3,19 @@ import satellite_images_nso._manipulation.nso_manipulator as nso_manipulator
 
 def tranform_vector_to_pixel_df(path_to_vector):
     """
-        Public wrapper around the nso manipulator.
+        Public wrapper around the nso manipulator for transforming a vector to a pixel geoDataFrame.
+
+        @path_to_vector: Path to a .tiff file.
     """
-    return nso_manipulator.tranform_vector_to_pixel_df(path_to_vector )
+    return nso_manipulator.tranform_vector_to_pixel_df(path_to_vector)
+
+
+def crop_nso_satellite_image(shape_file, path_to_tiff_file, output_path):
+    """ 
+        Public wrapper around the nso manipulator to make a cut out of a NSO satellite image.
+
+        @oaram shape_file: Path to a geojson file.
+        @param path_to_tiff_file: Path to a tiff file to be cut.
+        @param output_path: Where the cut should be stored.
+    """
+    return nso_manipulator.__make_the_cut(shape_file, path_to_tiff_file, output_path)
