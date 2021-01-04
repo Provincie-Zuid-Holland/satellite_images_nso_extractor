f86dd87282b7cedf60c38cc81c73d43fade8f5e8
Winter <mr.de.winter@pzh.nl>
2021-01-04 23:06:41 +0100

    Added extra pictures

--- src/satellite_images_nso/api/sat_manipulator.py
+++ src/satellite_images_nso/api/sat_manipulator.py
@@ -1,7 +1,7 @@
 # Rename to nso_manipulator for more apporiate name.
 import satellite_images_nso._manipulation.nso_manipulator as nso_manipulator
 
-def tranform_vector_to_pixel_df(path_to_vector):
+def tranform_vector_to_pixel_gpdf(path_to_vector):
     """
         Public wrapper around the nso manipulator for transforming a vector to a pixel geoDataFrame.
 
