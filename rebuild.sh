f0ac9dad572ead31b3484f3164ab8b6042a496a0
Michael de Winter <mr.de.winter@pzh.nl>
2021-03-11 10:58:33 +0100

    Added missing readme notes and removed database insertion

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
-pip uninstall -y dist/satellite_images_nso-1.1.6-py3-none-any.whl
+pip uninstall -y dist/satellite_images_nso-1.1.9-py3-none-any.whl
 rm -rf dist/
 python setup.py bdist_wheel
-pip install dist/satellite_images_nso-1.1.6-py3-none-any.whl
+pip install dist/satellite_images_nso-1.1.9-py3-none-any.whl
