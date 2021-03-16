11c9300d9cf9e7c51001fcca921607ed3c63987c
Michael de Winter <mr.de.winter@pzh.nl>
2021-03-16 11:03:52 +0100

    requirements update, and new geopandas update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="satellite_images_nso", # Replace with your own username
-    version="1.1.11",
+    version="1.1.12",
     author="Michael de Winter",
     author_email="m.r.dewinter88@live.nl",
     description="Satellie extraction and cutter",
