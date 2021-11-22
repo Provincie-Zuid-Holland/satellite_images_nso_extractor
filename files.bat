a8fb2b298bb81b8f848a849ff0ab9e86df89f87b
MW-88 <34949864+MW-88@users.noreply.github.com>
2021-11-22 14:04:49 +0100

    Forgot file

---
+++ files.bat
@@ -0,0 +1,15 @@
+git reset HEAD src/main.py
+git checkout main.py
+git reset HEAD src/nso_main_tests.ipynb
+git checkout src/nso_main_tests.ipynb
+git reset HEAD src/nso_newfiles_test.ipynb
+git checkout src/nso_newfiles_test.ipynb
+git reset HEAD src/nso_notebook_example_pzh.ipynb
+git checkout src/nso_notebook_example_pzh.ipynb
+git reset HEAD src/nso_notebook_normalize.ipynb
+git checkout src/nso_notebook_normalize.ipynb
+git reset HEAD  src/pzh_recropping.ipynb
+git checkout  src/pzh_recropping.ipynb
+
+
+
