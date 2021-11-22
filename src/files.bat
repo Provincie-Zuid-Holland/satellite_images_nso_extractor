414ac5ea42035398d7d6469a0c7460a841ad6879
MW-88 <34949864+MW-88@users.noreply.github.com>
2021-11-22 14:49:29 +0100

    Errors in the readme

---
+++ src/files.bat
@@ -0,0 +1,22 @@
+git reset HEAD src/main.py
+git checkout main.py
+git reset HEAD src/nso_main_tests.ipynb
+git checkout src/nso_main_tests.ipynb
+git reset HEAD src/nso_newfiles_test.ipynb
+git checkout src/nso_newfiles_test.ipynb
+git reset HEAD src/nso_notebook_example_pzh.ipynb
+git checkout src/nso_notebook_example_pzh.ipynb
+git reset HEAD src/nso_notebook_normalize.ipynb
+git checkout src/nso_notebook_normalize.ipynb
+git reset HEAD  src/pzh_recropping.ipynb
+git checkout  src/pzh_recropping.ipynb
+
+del main.py
+del nso_main_tests.ipynb
+del nso_newfiles_test.ipynb
+del nso_notebook_example_pzh.ipynb
+del nso_notebook_normalize.ipynb
+del pzh_recropping.ipynb
+
+
+
