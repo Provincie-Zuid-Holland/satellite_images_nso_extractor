ebc7cdae4dc730e896304ad2102aeee1896e6166
Michael <mr.de.winter@pzh.nl>
2022-02-10 13:42:49 +0000

    Added wms retrieval

--- src/satellite_images_nso/_nso_data_extraction/nso_api.py
+++ src/satellite_images_nso/_nso_data_extraction/nso_api.py
@@ -1,3 +1,4 @@
+from urllib import response
 import requests
 from requests.auth import HTTPBasicAuth 
 import objectpath
@@ -112,6 +113,74 @@ def retrieve_download_links(georegion, user_n, pass_n, start_date , end_date , m
 
     #sys.stdout = save_stdout
     return set(links)
+
+
+def retrieve_wms_links(georegion, user_n, pass_n, start_date , end_date , max_meters, strict_region , max_diff ):
+    """
+        This functions retrieves download links for satellite image corresponding to the region in the geojson.
+
+        @param georegion: a polygon with the georegion.
+        @param start_date: From when satelliet date needs to be looked at.
+        @param end_date: the end date of the period which needs to be looked at
+        @param max_meters: Maximum resolution which needs to be looked at.
+        @param strict_region: A filter applied to links to only fully contain the region.
+        @param max_diff: The percentage that a satellite image has to have of the selected geojson region.
+        @return: the found download links.
+    """
+
+    # FOR DEBUGGING !!!!!!!This blocks the large print output from the request.
+    #save_stdout = sys.stdout
+    #sys.stdout = open(os.devnull, 'w')
+
+    geojson_coordinates = georegion
+   
+    url = 'https://api.satellietdataportaal.nl/v1/search'
+    myobj = { "type": "Feature","geometry":
+    {"type": "Polygon", "coordinates": geojson_coordinates },"properties": {"fields":{"geometry":"false"},"filters" :
+    { "datefilter":{ "startdate": start_date,"enddate" : end_date }, "resolutionfilter":{ "maxres" :max_meters, "minres" : 0 }} } }
+
+    headers = {'content-type': 'application/json'}
+    x = requests.post(url, auth = HTTPBasicAuth(user_n, pass_n), data = json.dumps(myobj) ,  headers=headers)  
+    reponse = json.loads(x.text)
+
+    logger.info("Got following request:")
+    logger.info(reponse)    # Check if valid reponse
+    if "features" not in reponse.keys():
+        print(reponse)
+        raise Exception("No valid response from NSO message:"+x.text)       
+    links = []
+    
+    for row in reponse['features']:
+        try:  
+            if row['services'] is not None:
+
+
+                print("Cloudcover check:")
+                # Check for cloudcoverage. TODO: Use a variable for it.
+                if row['properties']['cloudcover'] is None or float(row['properties']['cloudcover']) < 30.0 :
+                   # This checks to see if the geojson is in the full region. TODO: Make it optional and propertional
+                    print("Passed cloud check")
+                    check_region = False
+                    print("Going into region check:")
+                    try:
+  
+                       check_region = check_if_geojson_in_region(row, geojson_coordinates, max_diff)
+                    except Exception as e:
+                          print(str(e)+" This error can be normal!" )
+                          logger.info(str(e)+" This error can be normal!") 
+
+                    if  check_region == True or strict_region == False:
+                        print("Passed region check")                
+                        for download in row['properties']['downloads']:
+                            links.append([download['href'], row['services']['wms']['link']])
+                       
+                
+        except Exception as e: 
+            print(str(e)+" This error can be normal!" )
+            logger.info(str(e)+" This error can be normal!")
+
+    #sys.stdout = save_stdout
+    return links
 
 
 def download_link(link, absolute_path, user_n, pass_n, file_exists_check: bool = False):
