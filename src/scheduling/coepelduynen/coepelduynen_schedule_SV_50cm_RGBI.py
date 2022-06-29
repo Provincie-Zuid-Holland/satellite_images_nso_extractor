472261b8e41ecc033e06d15329c23cfdad1d37a8
Michael <mr.de.winter@pzh.nl>
2022-06-29 08:54:24 +0000

    Update on scheduling

--- src/scheduling/coepelduynen/coepelduynen_schedule_SV_50cm_RGBI.py
+++ src/scheduling/coepelduynen/coepelduynen_schedule_SV_50cm_RGBI.py
@@ -8,6 +8,7 @@ import glob
 import settings
 import logging
 from azure.storage.blob import ContainerClient
+import tqdm
 
 """ 
 
@@ -88,6 +89,85 @@ def filter_links(links, done_files):
               SV_links.append(link)
   return SV_links
 
+# Add extra channels.
+
+def generate_ndvi_channel(tile):
+        """
+        Generate ndvi channel from 2 bands.
+        
+        @param tile: rgbi tile to calculate to the NDVI from.
+        @return a NDVI channel.
+        """
+        print("Generating NDVI channel...")
+        red = tile[2]
+        nir = tile[3]
+        ndvi = []
+        for i in tqdm.tqdm(range(len(red))):
+            ndvi.append((nir[i]-red[i])/(nir[i]+red[i]+1e-10)*255)
+        return np.array(ndvi, dtype=np.uint8)
+
+def generate_vegetation_height_channel(vegetation_height_data, vegetation_height_transform, target_transform, target_width, target_height):
+        """
+        Function to convert a .tif, which was created from a .laz file, into a band which can be used in a raster file with other bands.
+        
+        @param vegetation_height_data: numpy array from the ahn .tif file.
+        @param vegetation_height_transform: transform from the ahn .tif meta data.
+        @param target_transform: transform from the satellite .tif meta data.
+        @param target_width: The width from the satellite .tif file.
+        @param target_height: The height from the satellite .tif file.
+        """
+        print("Generating vegetation height channel...")
+        channel = np.array([[0] * target_width] * target_height, dtype=np.uint8)
+        src_height, src_width = vegetation_height_data.shape[0], vegetation_height_data.shape[1]
+        for y in tqdm.tqdm(range(target_height)):
+            for x in range(target_width):
+                rd_x, rd_y = rasterio.transform.xy(target_transform, y, x)
+                vh_y, vh_x = rasterio.transform.rowcol(vegetation_height_transform, rd_x, rd_y)
+                if vh_x < 0 or vh_x >= src_width or \
+                    vh_y < 0 or vh_y >= src_height:
+                    continue
+                # print(rd_x, rd_y, x, y, vh_x, vh_y)
+                channel[y][x] = vegetation_height_data[vh_y][vh_x]
+        return channel
+
+def get_ahn_data(ahn_input_file):
+
+  inds = rasterio.open(ahn_input_file, 'r')        
+  vegetation_height_data = inds.read(1)
+  vegetation_height_transform = inds.meta["transform"]
+
+  return vegetation_height_data, vegetation_height_transform
+
+
+def add_height_NDVI(tif_input_file):
+
+  inds = rasterio.open(tif_input_file, 'r') 
+  meta = inds.meta
+  meta.update(count = 6)   
+  tile = inds.read() # TODO is this behaviour similar to inds.read() ? MW: yes
+  ndvi = generate_ndvi_channel(tile)
+  #normalized_tile = np.array(normalise(tile, channel_normalisation, meta["width"], meta["height"]))  
+   
+  vegetation_height_data, vegetation_height_transform = get_ahn_data(settings.input_ahn_file)
+  heightChannel = generate_vegetation_height_channel(vegetation_height_data, vegetation_height_transform, inds.meta["transform"], meta["width"], meta["height"])
+  tile = np.append(tile, [heightChannel], axis=0)
+  tile = np.append(tile, [ndvi], axis=0)
+          
+  file_to = tif_input_file.replace(".tif","_ndvi_height.tif")#.split("/")[-1]
+  print(file_to)
+  
+ 
+  with rasterio.open(file_to, 'w', **meta) as outds:        
+                outds.write_band(1,tile[0])
+                outds.write_band(2,tile[1])
+                outds.write_band(3,tile[2])
+                outds.write_band(4,tile[3])
+                outds.write_band(5,ndvi)
+                outds.write_band(6,heightChannel)
+
+  return file_to      
+  
+
 if __name__ == '__main__':
 
   links,georegion = init()
@@ -101,7 +181,9 @@ if __name__ == '__main__':
     for link in SV_links:
       logger.info(link)
       cropped_location, no, no2 = georegion.execute_link(link, calculate_nvdi = False)
+      logger.info('Adding new channels')
+      file_all_channels = add_height_NDVI(cropped_location)
       logger.info("Uploading")
-      upload_file_rm_blob(cropped_location, cropped_location.split("/")[-1])
+      upload_file_rm_blob(file_all_channels, file_all_channels.split("/")[-1])
   else:
       logger.info("No new links")
