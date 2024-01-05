c769dadd7b6ba176d6610446b2d4a4e830e53212
Pieter Kouyzer <paj.kouyzer@pzh.nl>
2024-01-05 11:36:27 +0000

    Add settings and extend README
    Correct settings example

---
+++ src/settings_example.py
@@ -0,0 +1,11 @@
+# get credentials
+nso_username = ""
+nso_password = ""
+
+# give path to geojson, height_band (ahn) file, output path folder and cloud_detection_model_path
+path_geojson = ""
+height_band_filepath = ""
+output_path = ""
+cloud_detection_model_path = None
+
+links_must_contain = []  # i.e. ["SV", "50cm", "RGBI", "2022"]
