74830324f3539670d85bd6550700cad269343c4a
MW-88 <m.r.dewinter88@live.nl>
2024-05-06 14:24:33 +0000

    Further implementation of comments

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -7,6 +7,12 @@ from matplotlib import pyplot as plt
 
 
 def plot_tif(raster_path):
+    """
+    Function for plotting a .tif file.
+    Might be moved somewhere else.
+
+    @param raster_path: Path to a raster file 
+    """
         src = rasterio.open(raster_path)
         plot_out_image = np.clip(src.read()[2::-1],
                         0,2200)/2200 # out_image[2::-1] selects the first three items, reversed
