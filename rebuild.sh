2dc372c5f51b055e804baf67d2ed6816679ece7f
MW-88 <m.r.dewinter88@live.nl>
2024-07-16 10:24:53 +0000

    Refactor correct package name

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
-pip uninstall -y dist\satellite_images_nso-2.0.0-py3-none-any.whl
+pip uninstall -y dist\satellite_images_nso_extractor-2.0.0-py3-none-any.whl
 rm -rf dist/
 python -m build
-pip install dist\satellite_images_nso-2.0.0-py3-none-any.whl
+pip install dist\satellite_images_nso_extractor-2.0.0-py3-none-any.whl
