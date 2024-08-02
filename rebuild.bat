40d31c77031ceae867ad4a78dee18549f40cc85b
MW-88 <m.r.dewinter88@live.nl>
2024-08-02 08:55:48 +0000

    Correct way of installing with pip install -e

--- rebuild.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-pip uninstall -y dist\satellite_images_nso_extractor-2.0.0-py3-none-any.whl
-del /Q dist\
-python -m build
-pip install dist\satellite_images_nso_extractor-2.0.0-py3-none-any.whl
